0b602de0b84607d816515070074d4eb61054fe70
Sofia Benavides <Sbenavidesarias@usf.edu>
2025-09-11 18:33:48 -0400

    Fix Typos Step 2

--- cc_3.js
+++ cc_3.js
@@ -1,9 +1,21 @@
 // Coding Challenge 03a
 
 // Customer Records
-[
-    {name: "Maria", email: "Maria.Gonzalez@gmail", purchases: item:["HOODIE", "SHIRT", "SHOES"]}
-    {name: "Ashley", email: "Ashley.Wilson@gmail", purchases: item: ["HOODIE", "SHIRT"]}
-    {name: "John", email: "John.Martinez@gmail", purchases: item: ["HOODIE"]}
+let customer = [
+    {
+        name: "Maria",
+        email: "Maria.Gonzalez@gmail",
+        purchases: ["HOODIE", "SHIRT", "SHOES"]
+    },
+    {
+        name: "Ashley", 
+        email: "Ashley.Wilson@gmail", 
+        purchases: ["HOODIE", "SHIRT"]
+    },
+    {
+        name: "John", 
+        email: "John.Martinez@gmail", 
+        purchases: ["HOODIE"]
+    }
 ]
 
