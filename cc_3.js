161d04e7f029ed8d695bed3b7f1f613eaa106267
Sofia Benavides <Sbenavidesarias@usf.edu>
2025-09-11 18:27:18 -0400

    Initial Commit

---
+++ cc_3.js
@@ -0,0 +1,9 @@
+// Coding Challenge 03a
+
+// Customer Records
+[
+    {name: "Maria", email: "Maria.Gonzalez@gmail", purchases: item:["HOODIE", "SHIRT", "SHOES"]}
+    {name: "Ashley", email: "Ashley.Wilson@gmail", purchases: item: ["HOODIE", "SHIRT"]}
+    {name: "John", email: "John.Martinez@gmail", purchases: item: ["HOODIE"]}
+]
+
