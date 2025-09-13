04e3143ad484852abd5c9f1e74f278ee33667002
Sofia Benavides <Sbenavidesarias@usf.edu>
2025-09-12 23:33:56 -0400

    Customer Details

--- cc_3.js
+++ cc_3.js
@@ -26,4 +26,13 @@ customers.push({
 });
 // Add/ Remove Email and purchases
 customers[0].email = "Maria.Gonzalez@gmail.com"
+
+// Customer Details
 customers[1].purchases.push("SHOES");
+    customers.forEach(customer => {
+        console.log(`Name: ${customer.name}`);
+        console.log(`Email: ${customer.email}`);
+    console.log(`Total Purchases: ${customer.purchases.length}`);
+    console.log('-------------------');
+    });
+
